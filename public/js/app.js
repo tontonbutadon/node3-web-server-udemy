245becb36d0d146027d07b02e2b103c03a51b825
tontonbudadon <rei_yasuda@zenb.jp>
2023-03-14 07:59:38 +0900

    Remove unnecessary row

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,6 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
   })  
 })
 */
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
